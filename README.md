dfd0ce3b63535aabed17216d2d54ef8844510bd6
Rcjot <rcappy495@gmail.com>
2024-05-29 22:13:42 +0800

    buttons and popping divs working quite good.

--- README.md
+++ README.md
@@ -1,18 +1,11 @@
 # Project-Rock-Paper-Scissors
 
 
-i still don't know how to properly make a pseudocode, maybe im just lazy...
-
-my pseudocode:
-/*
-
-prompt user for move
-
-rng computer for move
-
-compare moves and decide winner
-
-declare winner declare score
+worked in branch....
 
 
-*/
+basically gethumanchoice() function is gone, replaced with three eventlistener buttons rock paper scissors...
+
+had trouble with how to set things up...
+
+things aren't looking good so far without the layout, will do later.
--- script.js
+++ script.js
@@ -1,11 +1,4 @@
 
-function getHumanChoice(round){
-
-    let choice = prompt('Round '+ round + '\nRock, Paper or Scissors?').toLowerCase();
-    console.log("you chose " + choice);
-
-    return choice;
-}
 
 
 function getComputerChoice(){
@@ -30,72 +23,108 @@ function getComputerChoice(){
 function playRound(human, computer){
     if (human === computer){
 
-        alert("you chose "+ human +"\ncomputer chose "+computer+"\nyou both chose the same move");
+        console.log("you chose "+ human +"\ncomputer chose "+computer+"\nyou both chose the same move");
         return 'draw';
     }else if (human === 'scissors' && computer === 'paper'){
-        alert("you chose "+ human +"\ncomputer chose "+computer+"\nScissors beats Paper, You Win!");
+        console.log("you chose "+ human +"\ncomputer chose "+computer+"\nScissors beats Paper, You Win!");
         return 'win';
     }else if (human === 'scissors' && computer === 'rock'){
-        alert("you chose "+ human +"\ncomputer chose "+computer+"\nRock beats Scissors, You Lose!");
-        return 'win';
+        console.log("you chose "+ human +"\ncomputer chose "+computer+"\nRock beats Scissors, You Lose!");
         return 'lose';
     }else if (human === 'paper' && computer === 'rock'){
-        alert("you chose "+ human +"\ncomputer chose "+computer+"\nPaper beats Rock, You Win!");
+        console.log("you chose "+ human +"\ncomputer chose "+computer+"\nPaper beats Rock, You Win!");
         return 'win';
     }else if (human === 'paper' && computer === 'scissors'){
-        alert("you chose "+ human +"\ncomputer chose "+computer+"\nScissors beats Paper, You Lose!");
+        console.log("you chose "+ human +"\ncomputer chose "+computer+"\nScissors beats Paper, You Lose!");
         return 'lose';
     }else if (human === 'rock' && computer === 'scissors'){
-        alert("you chose "+ human +"\ncomputer chose "+computer+"\nRock beats Scissors, You Win!");
+        console.log("you chose "+ human +"\ncomputer chose "+computer+"\nRock beats Scissors, You Win!");
         return 'win';
     }else if (human === 'rock' && computer === 'paper'){
-        alert("you chose "+ human +"\ncomputer chose "+computer+"\nPaper beats Rock, You Lose!");
+        console.log("you chose "+ human +"\ncomputer chose "+computer+"\nPaper beats Rock, You Lose!");
         return 'lose';
     }else{
-        alert("Incorrect Input! Therefore you LOSE!");
+        consolo.log("Incorrect Input! Therefore you LOSE!");
         return 'lose';
     }
 }
 
+//results class reference
+const results = document.querySelector(".results")
+//current score div
+const currentScore = document.createElement("div");
+results.appendChild(currentScore);
+
+let humanscore = 0;
+let computerscore = 0;
 
 
-function playGame(){
-    let game = true;
-    while(game){
-        let humanscore = 0;
-        let computerscore = 0;
-        
-    
-        alert("Play Rock Paper Scissors! \ndoes backslash n work?");
-        for (let rounds = 1; rounds < 6; rounds++){
-            let WL = playRound(getHumanChoice(rounds), getComputerChoice());
-    
-            if (WL === 'win'){
-                humanscore++;
-            }else if (WL === 'lose'){
-                computerscore++;
-            }
-            console.log("human: " + humanscore + " computer: " + computerscore);
-            alert("You: " + humanscore + " Computer: " + computerscore);
-        }
 
-        let Won = humanscore>computerscore ? 'won!' : humanscore<computerscore ? 'lost!' : 'tied!';
-        alert("GAME OVER\nFinal Score:\nYou: " + humanscore + " \nComputer: " + computerscore +"\nYou " + Won);
-
-        let play = prompt("Game ended!\nWant to play again?\ntype 'yes' or 'no'").toLowerCase();
-        if (play === 'yes'){
-            game = true;
-        }else {
-            game = false;
-        }
-
+function playGame(choice){
+    console.log("playGame initiataedad");
+  
+    let WinOrLose = playRound(choice, getComputerChoice());
+    if (WinOrLose === 'win'){
+        humanscore++;
+    }else if (WinOrLose === 'lose'){
+        computerscore++;
     }
+    
+    currentScore.textContent = "You: " + humanscore + " computer: " + computerscore;
+    
+
+    if (humanscore === 5 || computerscore === 5){
+        let Won = humanscore>computerscore ? 'won!' : humanscore<computerscore ? 'lost!' : 'tied!';
+        let myText = "GAME OVER!   Final Score: You: "+humanscore + "  Computer: " + computerscore +"\nYou " + Won;  
+        // put a button to play again!
+        const resultsText = document.createElement("div");
+        resultsText.textContent = myText;
+        results.appendChild(resultsText);
+        humanscore = 0;
+        computerscore = 0;
+    }
+    
+
+    // if (typeof choice === 'undefined'){
+    //     console.log("please pick a move!");
+    // }else{
+        
+    //     let WL = playRound(getHumanChoice(), getComputerChoice());
 
 
+
+
+        
+    // }
 }
 
-console.log("Hi!");
-playGame();
+//button listen to events!
+let choice = '';
+
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissors = document.querySelector(".scissors");
+
+rock.addEventListener("click", () => {
+    console.log("roock");
+    choice = 'rock';
+    playGame(choice);
+});
+paper.addEventListener("click", () => {
+    console.log("papperr");
+    choice = 'paper';
+    playGame(choice);
+});
+scissors.addEventListener("click", () => {
+    console.log("scisosors");
+    choice = 'scissors';
+    playGame(choice);
+});
+
+
+
+// console.log("Hi!");
+// playGame();
 
 
 
